631c70b3cbd2f47738aba9979947a6db436874b3
fer <fernandoza@gmail.com>
2017-12-12 14:23:00 -0600

    Solve the order by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,19 @@
+USE employees;
+
+# Find all employees with first names 'Irena', 'Vidya', or 'Maya'
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC , first_name DESC ;
+
+
+# Find all employees whose last name starts with 'E'
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+order by emp_no DESC ;
+
+# Find all employees hired in the 90s and born on Christmas
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25'
+order by birth_date ASC, hire_date DESC;;
