e354b19f70fa32655e4bd4bfda236604d3668fec
Luis Montealegre <montealegreluis@gmail.com>
2017-12-13 11:52:57 -0600

    Add examples using GROUP BY

--- group-by-examples.sql
+++ group-by-examples.sql
@@ -1,3 +1,22 @@
+-- Aggregate functions (functions with memory)
+-- count, avg, sum, max, min
 
-SELECT first_name
-FROM employees;
+SELECT
+  count(*),
+  first_name,
+  last_name
+FROM employees
+WHERE first_name LIKE '%z%'
+GROUP BY first_name, last_name
+ORDER BY count(*) DESC;
+
+SELECT sum(salary), to_date -- projection
+FROM salaries
+WHERE to_date = '9999-01-01' -- current salaries
+GROUP BY to_date;
+
+
+SELECT max(salary), to_date -- projection
+FROM salaries
+WHERE to_date = '9999-01-01' -- current salaries
+GROUP BY to_date;
